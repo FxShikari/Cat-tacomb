009bfe8dbc0a69e9054768a5c912a88141c76fa0
jean <95246204+Raphael1199@users.noreply.github.com>
2026-06-12 10:05:54 +0400

    audio

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public AudioClip _musicChateau;
+    public AudioSource _audioChateau;
+
+    public AudioClip _musiCatacomb;
+    public AudioSource _audioCatacomb;
+    public static AudioManager Instance { get; private set; }
+    [SerializeField] private PlayerCharacter _character;
+
+    private void Awake()
     {
-        
+        // If there is an instance, and it's not me, delete myself.
+        DontDestroyOnLoad(this);
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void PlayMusic(int num)
     {
-        
+        if (num == 1)
+        {
+            _audioChateau.Play();
+        }
+        else if (num == 2)
+        {
+            _audioCatacomb.Play();
+        }
     }
 }
